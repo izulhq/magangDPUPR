3a1045263d7078b67db31c5ddbd24ae4e7fc8419
izulhq <55051387+izulhq@users.noreply.github.com>
2024-08-02 11:33:18 +0700

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -621,21 +621,6 @@ var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
     document.title = getSetting('_mapTitle');
     addBaseMap();
 
-    // Tambahan Baru
-    var parsedPoints = parse(points);
-    var pointGeoJSON = pointsToGeoJSON(parsedPoints);
-
-    var searchControl = new L.Control.Search({
-      layer: L.geoJson(pointsGeoJSON),
-      propertyName: 'Name',
-      marker: false,
-      moveToLocation: function(latlng, title, map) {
-        map.setView(latlng, 16);
-      }
-    });
-
-    map.addControl(searchControl);
-
     // Add point markers to the map
     var layers;
     var group = '';
@@ -844,27 +829,6 @@ var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
               }
             }
           }
-
-// Tambahan Baru 2
-function pointsToGeoJSON(points) {
-  return {
-    "type": "FeatureCollection",
-    "features": points.map(function(point) {
-      return {
-        "type": "Feature",
-        "properties": {
-          "name": points.Name,
-          "description": points.Description
-          // Tambahkan properti lain yang Anda butuhkan
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [parseFloat(points.Longitude), parseFloat(points.Latitude)]
-        }
-      };
-    })
-  };
-}
           
           var line = L.polyline(latlng, {
             color: (p[index]['Color'] == '') ? 'grey' : p[index]['Color'],
