81028be4f7a63b1ba0dc3525a0e5b2a3d1e04ca3
izulhq <55051387+izulhq@users.noreply.github.com>
2024-08-05 12:55:21 +0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1v69vBurHSufrEW0qiBaA3D5aNHE_QKNsFlO-XI-tOis/edit?gid=56209222#gid=56209222';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1v69vBurHSufrEW0qiBaA3D5aNHE_QKNsFlO-XI-tOis/';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
